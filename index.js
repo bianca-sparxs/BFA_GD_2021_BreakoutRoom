d63b1dccbb528889e2ca2f5f4bcaef7274a26fd2
bianca-sparxs <c-bianca2010@hotmail.com>
2021-05-06 09:17:00 -0400

    add poem page, p much done Todo: credit the person who gave us glyphworld

--- index.js
+++ index.js
@@ -133,6 +133,12 @@ function hideElement() {
     details.style.display = "none"
 }
 
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+  }
+
 
 // taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
@@ -199,6 +205,23 @@ function bido() { //start in igbo
         </div>`
 
     })
+
+    if (hasTouch()) { // remove all the :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
+    
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+            if (!styleSheet.rules[ri].selectorText) continue;
+    
+            if (styleSheet.rules[ri].selectorText.match(':hover')) {
+            styleSheet.deleteRule(ri);
+            }
+        }
+        }
+    } catch (ex) {}
+    }
 }
 
 
